a79907178ee0157b38ff9753cfc293f0d485913a
Deyvid Andrade <deyvid-asilva@hotmail.com>
2020-06-11 09:18:24 -0300

    Criacao da classe C

---
+++ src/C.java
@@ -0,0 +1,30 @@
+public class C {
+    private int C1;
+    private float C2;
+
+    public int getC1() {
+        return C1;
+    }
+
+    public void setC1(int c1) {
+        C1 = c1;
+    }
+
+    public float getC2() {
+        return C2;
+    }
+
+    public void setC2(float c2) {
+        C2 = c2;
+    }
+
+    public void MC1() {
+
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+
+        System.out.println("MC2");
+    }
+}
